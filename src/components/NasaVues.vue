359a4581a60ae379e660a75122d9671f2dc74556
bhayek12@gmail.com <bhayek12@gmail.com>
2019-01-07 19:34:29 -0700

    Add buttons for diferent endpoints

--- src/components/NasaVues.vue
+++ src/components/NasaVues.vue
@@ -5,9 +5,11 @@
     </header>
     <body class='main-page'>
       <aside>
-        <hr class='border'>
         <h3 class="aside-title">Can't get enough space? Here's more</h3>
-        <button class="curr-month">Current months pictures</button>
+        <button class="curr-month"><i class="fas fa-angle-double-right"></i> Current months pictures!</button>
+        <button class="curr-month"><i class="fas fa-angle-double-right"></i> Pictures from the Mars rover!</button>
+        <button class="curr-month"><i class="fas fa-angle-double-right"></i> Pictures of earth!</button>
+        <button class="curr-month"><i class="fas fa-angle-double-right"></i> Back to Pic of the day!</button>
       </aside>
       <section>
         <h1>Here is the NASA astronomy picture of the day</h1>
@@ -40,7 +42,7 @@ export default {
 <style scoped>
 .main-page {
   display: grid;
-  grid-template-columns: 15em 1fr;
+  grid-template-columns: 25em 1fr;
   background-color: black;
   height: 100vh;
 }
@@ -52,13 +54,61 @@ export default {
   background-repeat: no-repeat;
   height: 25vh;
 }
-hr.border {
-  width: 75%;
-  border-top: 1px solid #9E5BC9;
-  border-bottom: 1px solid #9E5BC9;
+.aside-title {
+  margin-bottom: 2em;
+  font-size: 1.75em;
+  text-align: center;
+}
+.fa-angle-double-right {
+  color: #9E5BC9;
 }
 .curr-month {
-  color: #9E5BC9;
+  border: none;
+  cursor: pointer;
+  color: white;
+  background-color: transparent;
+  font-size: 20px;
+  transition:800ms ease all;
+  border:none;
+  position:relative;
+  height:60px;
+  padding:0 2em;
+  cursor:pointer;
+  transition:800ms ease all;
+  outline:none;
+  margin-bottom: 1em;
+  width: 100%;
+  text-align: left;
+}
+
+.curr-month:hover{
+  background:#fff;
+  background: linear-gradient(to right,#9E5BC9);
+  -webkit-text-fill-color: transparent;
+  -webkit-background-clip: text;
+}
+
+.curr-month:before,button:after{
+  content:'';
+  position:absolute;
+  top:0;
+  right:0;
+  height:2px;
+  width:0;
+  background: linear-gradient(to right, #9E5BC9);    
+  transition:400ms ease all;
+}
+
+.curr-month:after{
+  right:inherit;
+  top:inherit;
+  left:0;
+  bottom:0;
+}
+
+.curr-month:hover:before,button:hover:after{
+  width:100%;
+  transition:800ms ease all;
 }
 .title {
   margin-left: 25px;
